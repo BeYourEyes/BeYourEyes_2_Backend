c4a2b5fe8595011445a90658754b31fa64a110e2
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-11 16:23:30 +0900

    refactor : 불필요한 import 제거

--- src/main/kotlin/com/beyoureyes/beyoureyes/dto/ResponseDto.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/dto/ResponseDto.kt
@@ -1,8 +1,6 @@
 package com.beyoureyes.beyoureyes.dto
 
 import com.beyoureyes.beyoureyes.utils.ResponseStatus
-import lombok.Getter
-import lombok.Setter
 
 data class ResponseDto<T>(
     val status: ResponseStatus,
