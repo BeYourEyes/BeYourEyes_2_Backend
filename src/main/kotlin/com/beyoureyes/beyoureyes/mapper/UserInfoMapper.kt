ee48e070401ca86d993d9d1a337e13cefeb53090
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-18 20:41:01 +0900

    feat : UserInfo 테이블 없으면 생성하도록 구현

--- src/main/kotlin/com/beyoureyes/beyoureyes/mapper/UserInfoMapper.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/mapper/UserInfoMapper.kt
@@ -5,9 +5,23 @@ import org.apache.ibatis.annotations.Insert
 import org.apache.ibatis.annotations.Mapper
 import org.apache.ibatis.annotations.Options
 import org.apache.ibatis.annotations.Select
+import org.apache.ibatis.annotations.Update
 
 @Mapper
 interface UserInfoMapper {
+
+    @Update("""
+        CREATE TABLE IF NOT EXISTS userInfo (
+            user_info_id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
+            user_birth TIMESTAMP NOT NULL,
+            user_gender TINYINT NOT NULL,
+            user_nickname VARCHAR(255) NOT NULL,
+            CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+        )
+    """)
+    fun createTableIfNotExists()
+
     @Insert("""
         INSERT INTO userInfo (user_id, user_birth, user_gender, user_nickname)
         VALUES (#{userId}, #{userBirth}, #{userGender}, #{userNickname})
