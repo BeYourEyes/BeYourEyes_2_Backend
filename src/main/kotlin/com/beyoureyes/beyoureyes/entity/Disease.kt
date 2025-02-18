5ba802b9e9054d24be003d095360192dd929f2f3
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-18 20:04:31 +0900

    feat : Disease 엔터티 추가

---
+++ src/main/kotlin/com/beyoureyes/beyoureyes/entity/Disease.kt
@@ -0,0 +1,9 @@
+package com.beyoureyes.beyoureyes.entity
+
+data class Disease (
+    val diseaseId : Long? = null,
+    val userId : Long,
+    val diabetes : Boolean = false, // 당뇨병
+    val hypertension : Boolean = false, // 고혈합
+    val hyperlipidemia : Boolean = false // 고지혈증
+)
