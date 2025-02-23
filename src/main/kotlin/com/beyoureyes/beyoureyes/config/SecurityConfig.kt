e3285831fbf8e5d0cadb6bda302f75fc04e8df93
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-23 14:16:31 +0900

    feat : 시큐리티 검증에 jwt 토큰 검증 추가

--- src/main/kotlin/com/beyoureyes/beyoureyes/config/SecurityConfig.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/config/SecurityConfig.kt
@@ -1,22 +1,39 @@
 package com.beyoureyes.beyoureyes.config
 
+import com.beyoureyes.beyoureyes.jwt.JwtAuthenticationFilter
+import com.beyoureyes.beyoureyes.jwt.JwtUtil
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(private val jwtUtil: JwtUtil) {
     @Bean
     fun securityFilterChain(http: HttpSecurity) : SecurityFilterChain {
         http
-            .authorizeHttpRequests { auth -> auth
-                    .requestMatchers("/", "/user/login", "/user/verify-token").permitAll()
-                .anyRequest().authenticated()
-            }
             .csrf { it.disable() }
             .cors { }
+            .sessionManagement{it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)}
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/", "/user/login", "/user/verify-token").permitAll()
+                    .requestMatchers("/user-save").authenticated()
+                    .anyRequest().permitAll()
+            }
+
+            .addFilterBefore(JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter::class.java)
+
 
         return http.build()
+    }
+
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration) : AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
     }
 }
