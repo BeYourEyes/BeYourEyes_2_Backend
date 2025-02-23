1e7b5c21951856f12880e83f2f8b2a72ebaf5fa9
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-23 13:50:29 +0900

    fix : UserMapper 소괄호 오류 수정

--- src/main/kotlin/com/beyoureyes/beyoureyes/mapper/UserMapper.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/mapper/UserMapper.kt
@@ -18,7 +18,7 @@ interface UserMapper {
     @Select("SELECT user_id, device_id FROM user")
     fun findAll() : List<User>
 
-    @Select("SELECT user_id FROM user WHERE device_id = #{deviceId")
+    @Select("SELECT user_id FROM user WHERE device_id = #{deviceId}")
     fun findByDeviceId(deviceId : String) : User?
 
     @Insert("INSERT INTO user (device_id) VALUES (#{deviceId})")
--- src/main/kotlin/com/beyoureyes/beyoureyes/service/DatabaseService.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/service/DatabaseService.kt
@@ -2,6 +2,7 @@ package com.beyoureyes.beyoureyes.service
 
 import com.beyoureyes.beyoureyes.mapper.AllergyMapper
 import com.beyoureyes.beyoureyes.mapper.DiseaseMapper
+import com.beyoureyes.beyoureyes.mapper.UserInfoMapper
 import com.beyoureyes.beyoureyes.mapper.UserMapper
 import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Service
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Service
 @Service
 class DatabaseService(
     private val userMapper : UserMapper,
+    private val  userInfoMapper: UserInfoMapper,
     private val allergyMapper: AllergyMapper,
     private val diseaseMapper: DiseaseMapper
 ) {
     @PostConstruct
     fun init() {
         userMapper.createTableNotExists()
+        userInfoMapper.createTableIfNotExists()
         allergyMapper.createTableIfNotExists()
         diseaseMapper.createTableIfNotExists()
     }
